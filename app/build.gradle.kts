c0ea573588f194e5f0a588b5be85d958ba2d63b4
ee212943 <ee212943@sasken.com>
2025-02-08 00:30:50 +0530

    implemented sample counter operation upon click of 2 buttons

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.joynext.sasken.myjenkinproject"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.joynext.sasken.myjenkinproject"
         minSdk = 29
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -32,6 +32,10 @@ android {
     }
     kotlinOptions {
         jvmTarget = "1.8"
+    }
+    buildFeatures{
+        viewBinding = true
+        dataBinding = true
     }
 }
 
--- app/src/main/java/com/joynext/sasken/myjenkinproject/MainActivity.kt
+++ app/src/main/java/com/joynext/sasken/myjenkinproject/MainActivity.kt
@@ -2,10 +2,29 @@ package com.joynext.sasken.myjenkinproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.joynext.sasken.myjenkinproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val counterTV = binding.counter
+
+        binding.add.setOnClickListener {
+            val currentVal = counterTV.text.toString().toInt()
+            if (currentVal < 100) {
+                counterTV.text = (currentVal + 1).toString()
+            }
+        }
+
+        binding.subtract.setOnClickListener {
+            val currentVal = counterTV.text.toString().toInt()
+            if (currentVal > 0) {
+                counterTV.text = (currentVal - 1).toString()
+            }
+        }
     }
 }
